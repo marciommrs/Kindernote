ee53a32af262753a17367ce6edb3ef66d22bbaf8
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-07-26 12:49:12 -0300

    Adicionar background white aos componentes.

--- frontend/src/components/TextField/index.js
+++ frontend/src/components/TextField/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import MaterialTextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './styles.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'white'
+  },
+}));
+
 export default function TextField(props) {
+  const classes = useStyles();
 
   return (
     <>
@@ -17,7 +25,8 @@ export default function TextField(props) {
           type={props.type}
           InputLabelProps={{
             shrink: true,
-          }}/>
+          }}
+          className={classes.root}/>
     </>
   );
 }
