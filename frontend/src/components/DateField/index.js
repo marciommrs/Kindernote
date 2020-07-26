94da12642580fb40d7a2433c2409d703d05f66ca
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-07-26 12:48:25 -0300

    Adicionar background white ao DateField

--- frontend/src/components/DateField/index.js
+++ frontend/src/components/DateField/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import MaterialTextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './styles.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'white'
+  },
+}));
+
 export default function DateField(props) {
+  const classes = useStyles();
 
   return (
     <>
@@ -17,7 +25,8 @@ export default function DateField(props) {
           type="date"
           InputLabelProps={{
             shrink: true,
-          }}/>
+          }}
+          className={classes.root}/>
     </>
   );
 }
